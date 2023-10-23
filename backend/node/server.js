d96f5d8daac782b3bb9c48785671388c9b0e4b73
RamonBernaus <a21xavcabmil@inspedralbes.cat>
2023-10-23 13:27:41 +0200

    Login començat

--- backend/node/server.js
+++ backend/node/server.js
@@ -2,14 +2,34 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 //const mysql = require('mysql');
-const PORT = 3000
+const PORT = 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+let users = {
+    username: "Ramon",
+    password: "123456"
+}
+
 server.listen(PORT, function () {
     console.log("Server running on port " + PORT);
+});
+
+
+app.get('/api/Login/:username/:password', function (req, res) {
+    const username = req.params.username;
+    const password = req.params.password;
+    if (username === users.username && password === users.password) {
+        res.send("Bienvenido");
+    } else if (username === users.username && password ==! users.password) {
+        res.send("Error, la contrasenya es incorrecta");
+    } else if(username==! users.username && password === users.password){
+        res.send("Error, el usuari no es correcte");
+    } else if(username ==! users.username && password ==! users.password){
+        res.send("Error, les variables user i password son incorrectes")
+    }
 });
 
 // Configuració de la conexió a la base de dades
