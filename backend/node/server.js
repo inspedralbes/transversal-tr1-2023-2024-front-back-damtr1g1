8cc2684d7e090e440bb9401cb55c6e97b02b8654
RamonBernaus <a21xavcabmil@inspedralbes.cat>
2023-10-24 13:14:37 +0200

    ShoppingCart Data

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+//const socketIo = require('socket.io');
 const mysql = require('mysql');
 const PORT = 3000;
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const bodyParser = require('body-parser');
 
-server.listen(PORT, function () {
+const app = express();
+app.use(bodyParser.json());
+
+//const io = socketIo(server);
+
+app.listen(PORT, function () {
     console.log("Server running on port " + PORT);
 });
 
@@ -67,11 +69,37 @@ app.get('/api/validacioLogin', (req, res) => {
             return res.status(200).json({ Boolean: true });
         } else {
             // Si el usuario y la contraseña no coinciden, devuelve un mensaje de error
-            return res.status(401).json({ Boolean: false });
+            return res.status(401).json({ Boolean: false + "Error" });
         }
     });
 });
 
+// Enviar els poductes de la DB al client 
+app.post('/api/ShoppingCartData', (req, res) => {
+    connection.query('SELECT * FROM Usuarios', (error, results, fields) => {
+        if (error) {
+            // Errors 
+            return res.status(500).json({ error: 'Ocurrió un error al consultar la base de datos.' });
+        } else {
+            return res.json(results)
+        }
+    })
+})
+
+
+// Eliminar productes de la DB y eniviar els canvis
+app.post('/api/EliminarData', (req, res) => {
+    const productId = req.body.id; // El client ens envia el id del producte
+
+    const query = 'DELETE FROM Productos WHERE id = ?'; // Borrem el producte que ens han dit
+    connection.query(query, [productId], (error, results, fields) => {
+        if (error) {
+            res.status(500).json({ message: 'Error al eliminar el producto.' });
+        } else {
+            res.status(200).json({ message: `Producto con ID ${productId} eliminado correctamente.` });
+        }
+    });
+});
 
 // Código para cerrar la conexión cuando el servidor se cierre
 process.on('SIGINT', () => {
