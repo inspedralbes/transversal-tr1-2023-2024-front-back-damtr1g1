4d7a81b4f506e730ae8c8bb1d47b31edb0f528d8
RamonBernaus <a21xavcabmil@inspedralbes.cat>
2023-10-24 09:10:25 +0200

    Conectat a la DB

--- backend/node/server.js
+++ backend/node/server.js
@@ -1,64 +1,48 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-//const mysql = require('mysql');
+const mysql = require('mysql');
 const PORT = 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-let users = {
-    username: "Ramon",
-    password: "123456"
-}
-
 server.listen(PORT, function () {
     console.log("Server running on port " + PORT);
 });
 
-
-app.get('/api/Login/:username/:password', function (req, res) {
-    const username = req.params.username;
-    const password = req.params.password;
-    if (username === users.username && password === users.password) {
-        res.send("Bienvenido");
-    } else if (username === users.username && password ==! users.password) {
-        res.send("Error, la contrasenya es incorrecta");
-    } else if(username==! users.username && password === users.password){
-        res.send("Error, el usuari no es correcte");
-    } else if(username ==! users.username && password ==! users.password){
-        res.send("Error, les variables user i password son incorrectes")
-    }
-});
+app.get('/', function (req, res) {
+    res.send("Conectat al server")
+})
 
 // Configuració de la conexió a la base de dades
-/* const dbConfig = {
-    host: 'localhost',
-    user: 'tu_usuario',
-    password: 'tu_contraseña',
-    database: 'nombre_de_tu_base_de_datos'
-}; */
+const dbConfig = {
+    host: 'dam.inspedralbes.cat',
+    user: 'a22tomybanog_Projecte1',
+    password: 'Projecte1',
+    database: 'a22tomybanog_Projecte1'
+};
 
 // Crear la connexió a la base de datos
-// const connection = mysql.createConnection(dbConfig);
+const connection = mysql.createConnection(dbConfig);
 
 // Conectar a la base de dades
-/*connection.connect((err) => {
+connection.connect((err) => {
     if (err) {
         console.error('Error al conectar a la base de datos: ' + err.stack);
         return;
     }
     console.log('Conexión a la base de datos exitosa');
-});*/
+});
 
 // Ruta per a obtenir dades de la base de dades
-/*app.get('/api/datos', (req, res) => {
-    connection.query('SELECT * FROM nombre_de_tu_tabla', (error, results, fields) => {
+app.get('/api/dadesDB', (req, res) => {
+    connection.query('SELECT * Usuarios', (error, results, fields) => {
         if (error) throw error;
-        res.send(results);
+        res.send(JSON.stringify(result));
     });
-});*/
+});
 
 
 io.on('connection', (socket) => {
@@ -72,6 +56,3 @@ io.on('connection', (socket) => {
         console.log('Un usuario se ha desconectado');
     });
 });
-
-
-
