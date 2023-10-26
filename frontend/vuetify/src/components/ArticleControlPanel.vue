4a593440ec530cf8de9212861d67addcb982d988
Marti-SV <a21marsalval@inspedralbes.cat>
2023-10-26 04:06:22 +0200

    interficie principal del panell casi acabada

---
+++ frontend/vuetify/src/components/ArticleControlPanel.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+
+<script>
+</script>
+
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col v-for="item in 3" :key="item" sm="6" md="4" lg="3" cols="12">
+        <v-card
+          style="background: linear-gradient(#9094e9, #b0b8f1)"
+          class="text-center"
+          elevation="4"
+          rounded="xl"
+        >
+          <v-card-title>
+            <div
+              class="font-weight-bold text-center text-h3 py-6 text-truncate"
+              style="color:#3D3976"
+            >
+              Comandes
+            </div>
+            <v-img
+              height="200"
+              src="../assets/img/order_icon.png"
+              style="border-top: black"
+            >
+            </v-img>
+            <v-btn class="my-4" height="50px" width="170px" rounded style="background: linear-gradient(to left, #E8321A, #FF7A68); color:white"
+              >Entra
+            </v-btn>
+          </v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- frontend/vuetify/src/components/LandingPage.vue
+++ frontend/vuetify/src/components/LandingPage.vue
@@ -1,10 +1,15 @@
 <script setup>
+import ArticleControlPanel from "./ArticleControlPanel.vue";
 </script>
 
 <script>
-
 </script>
 
 <template>
-  Hola
+  <v-container>
+    <h1 class="text-center text-h2 pb-16 mt-16 pt-10 font-weight-bold">
+      Panell de Control
+    </h1>
+    <ArticleControlPanel />
+  </v-container>
 </template>
