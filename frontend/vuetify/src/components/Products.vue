83284294756a1200799e4e7c59630ed926286aad
Marti-SV <a21marsalval@inspedralbes.cat>
2023-10-30 12:01:29 +0100

    canvi estils add product

--- frontend/vuetify/src/components/Products.vue
+++ frontend/vuetify/src/components/Products.vue
@@ -9,9 +9,13 @@ export default {
     productes_originals: "",
     busqueda: "",
     carregant: false,
+    addProductText: window.innerWidth < 1100 ? "+" : "NOU PRODUCTE",
   }),
   mounted() {
     this.carregant = true;
+    window.addEventListener("resize", () => {
+      this.onResize()
+    });
     fetch("http://localhost:3001/api/getProducts")
       .then((response) => response.json())
       .then((data) => {
@@ -20,6 +24,15 @@ export default {
         this.carregant = false;
         console.log(data);
       });
+  },
+  methods: {
+    onResize() {
+      if (window.innerWidth < 1100) {
+        this.addProductText = "+";
+      } else {
+        this.addProductText = "NOU PRODUCTE";
+      }
+    },
   },
   watch: {
     busqueda: function () {
@@ -40,10 +53,10 @@ export default {
       <v-col cols="2">
         <v-sheet style="background-color: transparent">
           <v-btn
-            class="bg-light-green-lighten-2"
+            class="bg-light-green-lighten-2 text-md-body-2 text-lg-h6"
             height="63px"
             rounded="xl"
-            text="NOU PRODUCTE"
+            v-text="addProductText"
             block
             @click="$router.push('/afegir-producte')"
           ></v-btn>
@@ -88,7 +101,14 @@ export default {
           >
             <h1>No hi han productes amb aquest nom</h1>
           </div>
-          <v-col v-for="producte in productes" :key="producte.id" sm="6" md="4" lg="3" cols="12">
+          <v-col
+            v-for="producte in productes"
+            :key="producte.id"
+            sm="6"
+            md="4"
+            lg="3"
+            cols="12"
+          >
             <Product :data="producte" />
           </v-col>
         </v-row>
