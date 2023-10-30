b5db40808ecc77f399c37fce74ec8181b2ae12e9
Marti-SV <a21marsalval@inspedralbes.cat>
2023-10-30 12:05:27 +0100

    android shopActivity

---
+++ frontend/android/app/src/main/java/com/example/tr1_takeaway/loginService/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.example.tr1_takeaway.loginService;
+
+public class LoginResponse {
+    private static boolean Boolean;
+
+
+    public static boolean isBoolean() {
+        return Boolean;
+    }
+
+
+    public void setBoolean(boolean bool) {
+        this.Boolean = bool;
+    }
+}
---
+++ frontend/android/app/src/main/java/com/example/tr1_takeaway/ui/profile/ProfileFragment.java
@@ -0,0 +1,35 @@
+package com.example.tr1_takeaway.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.tr1_takeaway.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends Fragment {
+
+    private FragmentProfileBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProfileViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
