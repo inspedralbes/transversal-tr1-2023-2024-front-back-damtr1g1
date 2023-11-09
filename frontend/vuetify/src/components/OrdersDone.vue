1533f15fc485ab5695b713c804f38cf6c075ed59
Marti-SV <a21marsalval@inspedralbes.cat>
2023-11-09 10:09:42 +0100

    comandes fetes canviades

--- frontend/vuetify/src/components/OrdersDone.vue
+++ frontend/vuetify/src/components/OrdersDone.vue
@@ -1,59 +1,18 @@
-<script setup>
-import io from "socket.io-client";
-</script>
+<script setup></script>
 
 <script>
 export default {
   props: ["title", "img", "id"],
   data: () => ({
     jsonComandes: {},
-    socket: null,
-    switchsNotSelected: true,
-    responseData: null,
-    comandaActual: null,
   }),
   mounted() {
-    this.socket = io(import.meta.env.VITE_SOCKETS, {
-      cors: {
-        origin: "*",
-      },
-      path: "/node/",
-      transports: ["websocket"],
-    });
-
-    this.socket.on("json", (data) => {
-      this.jsonComandes = data;
-    });
-  },
-  methods: {
-    enviarMensaje() {
-      this.socket.emit("mensaje", "Hola servidor, soy un cliente");
-    },
-    canviComanda(index) {
-      this.comandaActual = index;
-      this.switchsNotSelected = true;
-      document.querySelectorAll(".v-list-item").forEach((item) => {
-        item.style =
-          "background-color: #7875df; width: 100%; height: 100px; border: 2px solid #473f94;";
+    fetch(import.meta.env.VITE_NODE_ROUTE + "getComandesFinalitzades")
+      .then((response) => response.json())
+      .then((data) => {
+        this.jsonComandes = data;
+        console.log(this.jsonComandes);
       });
-      document.getElementById("comanda-" + index).style =
-        "background-color: #9094e9; width: 100%; height: 100px; border: 4px solid #473f94;";
-    },
-    switchSelected(nProductes) {
-      setTimeout(() => {
-        document.querySelectorAll(".text-success").forEach((item, index) => {
-          if (index + 1 == nProductes) {
-            this.switchsNotSelected = false;
-          } else {
-            this.switchsNotSelected = true;
-          }
-        });
-      }, 1);
-    },
-    acabarComanda() {
-      this.socket.emit("eliminaComanda", this.comandaActual);
-      this.canviComanda(this.comandaActual + 1);
-    },
   },
 };
 </script>
@@ -62,21 +21,19 @@ export default {
   <div>
     <v-table
       fixed-header
-      height="500px"
+      height=""
       style="background-color: transparent; color: black; font-weight: bold"
     >
       <thead class="text-h4">
         <tr>
-          <th class="text-center">Unitats</th>
-          <th class="text-center">Producte</th>
-          <th class="text-center">Hora de recollida</th>
+          <th class="text-center">Id</th>
+          <th class="text-center">Nom d'usuari</th>
+          <th class="text-center">Hora d'encarrec</th>
         </tr>
       </thead>
-      <tbody class="text-h6">
-        <tr
-          align="center"
-        >
-          <td>
+      <tbody>
+        <tr v-for="comanda in jsonComandes" :key="comanda" class="text-h6">
+          <td align="center">
             <div
               class="text-center"
               style="
@@ -87,13 +44,11 @@ export default {
                 border-radius: 20px;
               "
             >
-              2
+              {{ comanda.id }}
             </div>
           </td>
-          <td>awa</td>
-          <td>
-            13:45
-          </td>
+          <td>{{ comanda.usuari }}</td>
+          <td>{{ comanda.data_comanda }}</td>
         </tr>
       </tbody>
     </v-table>
