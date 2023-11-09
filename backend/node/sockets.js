d9be3ec5189869f11ff7163f98cb4641d0d7bccf
Marti-SV <a21marsalval@inspedralbes.cat>
2023-11-09 13:26:16 +0100

    sockets hardcoded

--- backend/node/sockets.js
+++ backend/node/sockets.js
@@ -4,6 +4,124 @@ function EnviarJson(io) {
     console.log("Un usuario se ha conectado");
 
     let data = {
+      comandes: [
+        {
+          comanda: {
+            productes: [
+              {
+                nom: "patata",
+                unitats: 3,
+              },
+              {
+                nom: "pera",
+                unitats: 2,
+              },
+              {
+                nom: "poma verda",
+                unitats: 5,
+              },
+              {
+                nom: "poma verda",
+                unitats: 5,
+              },
+              {
+                nom: "poma verda",
+                unitats: 5,
+              },
+              {
+                nom: "poma verda",
+                unitats: 5,
+              },
+              {
+                nom: "poma verda",
+                unitats: 5,
+              },
+            ],
+            dia: "25/10/2023",
+            hora: "13:46",
+          },
+        },
+        {
+          comanda: {
+            productes: [
+              {
+                nom: "patata1",
+                unitats: 3,
+              },
+              {
+                nom: "pera",
+                unitats: 2,
+              },
+            ],
+            dia: "25/10/2023",
+            hora: "13:46",
+          },
+        },
+        {
+          comanda: {
+            productes: [
+              {
+                nom: "patata2",
+                unitats: 3,
+              },
+              {
+                nom: "pera",
+                unitats: 2,
+              },
+            ],
+            dia: "25/10/2023",
+            hora: "13:46",
+          },
+        },
+        {
+          comanda: {
+            productes: [
+              {
+                nom: "patata3",
+                unitats: 3,
+              },
+              {
+                nom: "pera",
+                unitats: 2,
+              },
+            ],
+            dia: "25/10/2023",
+            hora: "13:46",
+          },
+        },
+        {
+          comanda: {
+            productes: [
+              {
+                nom: "patata4",
+                unitats: 3,
+              },
+              {
+                nom: "pera",
+                unitats: 2,
+              },
+            ],
+            dia: "25/10/2023",
+            hora: "13:46",
+          },
+        },
+        {
+          comanda: {
+            productes: [
+              {
+                nom: "patata5",
+                unitats: 3,
+              },
+              {
+                nom: "pera",
+                unitats: 2,
+              },
+            ],
+            dia: "25/10/2023",
+            hora: "13:46",
+          },
+        },
+      ],
     };
 
     socket.emit("json", data); // Envia el JSON al cliente cuando se conecta
